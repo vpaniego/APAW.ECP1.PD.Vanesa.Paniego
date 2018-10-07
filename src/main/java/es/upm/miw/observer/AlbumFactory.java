8b4fa598b3ca2b77ff3e3c942fea500dd01d1438
Vanesa Paniego <vanesa.paniego@trappit.com>
2018-10-07 12:56:33 +0200

    issue#1. Patrón Observer junto Factory aplicados en las entidades AlbumFactory (observable) y ProgramaRadio (observer) junto con clase Test.

---
+++ src/main/java/es/upm/miw/observer/AlbumFactory.java
@@ -0,0 +1,46 @@
+package es.upm.miw.observer;
+
+import es.upm.miw.Album;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AlbumFactory extends Observable {
+
+    private static AlbumFactory factory = null;
+
+    private Map<String, Album> mapaAlbum;
+
+    private AlbumFactory() {
+        this.mapaAlbum = new HashMap<>();
+    }
+
+    public static AlbumFactory getInstance() {
+        if (AlbumFactory.factory == null) {
+            AlbumFactory.factory = new AlbumFactory();
+        }
+        return AlbumFactory.factory;
+    }
+
+    public Album getAlbum(String id) {
+        assert id != null;
+        assert this.mapaAlbum != null;
+
+        return this.mapaAlbum.get(id);
+    }
+
+     public void removeAlbum(String id) {
+        assert id != null;
+        assert this.mapaAlbum != null;
+
+        this.mapaAlbum.remove(id);
+    }
+
+    public void addAlbum(Album album) {
+        assert this.mapaAlbum != null;
+        this.mapaAlbum.put(album.getId(), album);
+
+        this.notifyObservers();
+    }
+
+}
